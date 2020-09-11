9d566af83e4cf5f2b2b483aa43176f73afc025d2
RealPeha <real.peha@gmail.com>
2020-09-11 15:15:01 +0300

    formatting timetable.json, change date format

--- utils.js
+++ utils.js
@@ -3,7 +3,7 @@ const getRandomItem = array => array[Math.floor(Math.random() * array.length)]
 const link = (title, href) => href ? `<a href="${href}">${title}</a>` : null
 
 const formatDate = (date) => {
-    return `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`
+    return `${date.getDate().toString().padStart(2, '0')}.${date.getMonth().toString().padStart(2, '0')}.${date.getFullYear().toString().padStart(2, '0')}`
 }
 
 module.exports = {
