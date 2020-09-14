338395bd9e7dd74d533c9f5b464849cf766b3c11
RealPeha <real.peha@gmail.com>
2020-09-14 09:51:12 +0300

    fixed logger for inline_query

--- src/bot.js
+++ src/bot.js
@@ -13,8 +13,12 @@ const bot = new Telegraf(process.env.TOKEN)
 
 bot.catch((e) => console.log('Bot catch: ', e))
 
-const logger = ({ message, from: { is_bot, language_code, ...from } }, next) => {
-    console.log(`[${formatDate(new Date())}] ${message.text} ${JSON.stringify(from)}`)
+const logger = ({ updateType, message, from: { is_bot, language_code, ...from } }, next) => {
+    if (updateType === 'inline_query') {
+        console.log(`[${formatDate(new Date())}] inline_query ${JSON.stringify(from)}`)
+    } else {
+        console.log(`[${formatDate(new Date())}] ${message.text} ${JSON.stringify(from)}`)
+    }
 
     return next()
 }
